90def1a6a9ce7b9048b3bc6537a60b3b694983d8
nakayamahiroshi164 <box.xx164@gmail.com>
2023-09-28 12:17:41 +0900

    ShippingAddress テーブル作成

---
+++ db/migrate/20230928030312_create_shipping_addresses.rb
@@ -0,0 +1,17 @@
+class CreateShippingAddresses < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shipping_addresses do |t|
+      t.references :purchase_record, null: false, foreign_key: true
+      t.string :post_code, null: false
+      t.integer :prefecture_id , null: false
+      t.string :city, null: false
+      t.string :address, null: false
+      t.string :building_name, null: false
+      t.string :telephone_number, null: false
+      t.timestamps
+    end
+  end
+end
+
+
+
